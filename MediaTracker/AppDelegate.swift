68abc08c4c47d1bf9d14b31fc45435ea43e5f71c
Dylan Elliott <dylan.elliott@gmail.com>
2019-02-14 00:29:18 +1100

    Add tab bar icons

--- MediaTracker/AppDelegate.swift
+++ MediaTracker/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Iconic
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	let userListNamespaceKey = "USER_LIST"
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		
+		FontAwesomeIcon.register()
 		
 		self.window = UIWindow(rootViewController: [
 			gamesViewController.inNavigationController,
@@ -31,6 +34,7 @@ extension AppDelegate {
 	var gamesViewController: StoredListViewController<IGDBGame> {
 		let gamesListViewController = StoredListViewController<IGDBGame>(namespace: userListNamespaceKey, searchRequestFactory: IGDBRequests.search)
 		gamesListViewController.title = "Games"
+		gamesListViewController.tabBarItem.image = Iconic.image(withIcon: .gamepadIcon, size: CGSize(width: 40, height: 30), color: .black)
 		return gamesListViewController
 	}
 	
@@ -43,23 +47,25 @@ extension AppDelegate {
 			searchViewModel: booksSearchViewModel,
 			namespace: userListNamespaceKey)
 		booksListViewController.title = "Books"
+		booksListViewController.tabBarItem.image = Iconic.image(withIcon: .bookIcon, size: CGSize(width: 40, height: 30), color: .black)
 		
 		return booksListViewController
 	}
 	
-	private func tmdbListViewController<T>(_ title: String, _ searchRequest: (@escaping (String) -> URLRequest)) -> StoredListViewController<T> {
+	private func tmdbListViewController<T>(_ title: String, _ icon: FontAwesomeIcon, _ searchRequest: (@escaping (String) -> URLRequest)) -> StoredListViewController<T> {
 		let tmdbSearchViewModel = MappingAPISearchViewModel<TMDBSearchResponse, T>(searchResultMapping: { $0.results }, searchRequestFactory: searchRequest)
 		let tmdbListViewController = StoredListViewController<T>(searchViewModel: tmdbSearchViewModel, namespace: "\(userListNamespaceKey)_\(title)")
 		tmdbListViewController.title = title
+		tmdbListViewController.tabBarItem.image = Iconic.image(withIcon: icon, size: CGSize(width: 40, height: 30), color: .black)
 		return tmdbListViewController
 	}
 	
 	var moviesListViewController: StoredListViewController<TMDBMovie> {
-		return tmdbListViewController("Movies", TMDBRequests.searchMovies)
+		return tmdbListViewController("Movies", .filmIcon, TMDBRequests.searchMovies)
 	}
 	
 	var tvShowListViewController: StoredListViewController<TMDBTVShow> {
-		return tmdbListViewController("TV", TMDBRequests.searchTV)
+		return tmdbListViewController("TV", .desktopIcon, TMDBRequests.searchTV)
 	}
 	
 }
---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'MediaTracker' do
+	pod 'Iconic', :git => 'https://github.com/zvving/Iconic.git', :branch => 'swift4'
+end
